9b7e331dbf27acd15a21a7982ae93a93c22290bd
o5 <pcdiebold@gmail.com>
2018-05-18 16:59:55 -0700

    Format using prettier

--- src/actions.js
+++ src/actions.js
@@ -1,31 +1,30 @@
-import { defaultState } from './reducer';
+import { defaultState } from './reducer'
 
 export const sendNextPoints = (nextPoints, maxArrayLength) => ({
-  type: 'SEND_NEXT_POINTS',
-  nextPoints,
-  maxArrayLength,
-});
+	type: 'SEND_NEXT_POINTS',
+	nextPoints,
+	maxArrayLength,
+})
 
 export const setNextPoint = (id, value) => ({
-  type: 'SET_NEXT_POINT',
-  id,
-  value,
-});
+	type: 'SET_NEXT_POINT',
+	id,
+	value,
+})
 
 export const runPeriodicGeneration = (interval, cutOff) => {
-	const maxArrLength = Math.floor(cutOff / interval);
+	const maxArrLength = Math.floor(cutOff / interval)
 
 	return (dispatch, getState) => {
-		const currState = getState();
-		const { nextPoints } = currState;
-		dispatch(sendNextPoints(nextPoints, maxArrLength));
-		dispatch(setNextPoint(0, defaultState.nextPoints[0]));
-		dispatch(setNextPoint(1, defaultState.nextPoints[1]));
-		dispatch(setNextPoint(2, defaultState.nextPoints[2]));
+		const currState = getState()
+		const { nextPoints } = currState
+		dispatch(sendNextPoints(nextPoints, maxArrLength))
+		dispatch(setNextPoint(0, defaultState.nextPoints[0]))
+		dispatch(setNextPoint(1, defaultState.nextPoints[1]))
+		dispatch(setNextPoint(2, defaultState.nextPoints[2]))
 
 		setTimeout(() => {
-			dispatch(runPeriodicGeneration(interval, cutOff));
-		}, interval);
-	};
-};
-
+			dispatch(runPeriodicGeneration(interval, cutOff))
+		}, interval)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-import './index.css';
+import './index.css'
 
-import { rootReducer } from './reducer.js';
-import { runPeriodicGeneration } from './actions.js';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { rootReducer } from './reducer.js'
+import { runPeriodicGeneration } from './actions.js'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 // currently set to a 10s interval and a 300s cutoff
-store.dispatch(runPeriodicGeneration(10000, 300*1000));
+store.dispatch(runPeriodicGeneration(10000, 300 * 1000))
 
-ReactDOM.render((
+ReactDOM.render(
 	<Provider store={store}>
 		<App />
-	</Provider>
-), document.getElementById('root'));
-registerServiceWorker();
+	</Provider>,
+	document.getElementById('root')
+)
+registerServiceWorker()
